fcebbc49cf15e4fd80ccb5f6fe90359b1047d903
jesusasc <jesusasilva958@gmail.com>
2022-06-02 22:57:28 -0500

    ejercicio 14,15,16

---
+++ meses.php
@@ -0,0 +1,3 @@
+<?php
+	echo "ene, feb, mar, abr, may, jun, jul, ago, sep, oct, nov, dic";
+?>
---
+++ usuario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="formget.php" method="get">
+    Nombre: <input type="text" name="nombre"><br>
+    Email: <input type="text" name="email"><br>
+    <input type="submit" value="Enviar">
+</form>
+Hola <?php isset($_GET["nombre"]) ? print $_GET["nombre"] : ""; ?><br>
+Tu email es: <?php isset($_GET["email"]) ? print $_GET["email"] : ""; ?>
+</body>
+</html>
